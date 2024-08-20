248c0b640baa30893b96c5f7cef19a904239ef2e
dodoveloper <davide.cor94@gmail.com>
2024-08-20 15:35:57 +0200

    improvement: better dialogue options

--- addons/skillcap_dialogue_nodes/nodes/dialogue_option_button.gd
+++ addons/skillcap_dialogue_nodes/nodes/dialogue_option_button.gd
@@ -1,19 +1,42 @@
+@tool
 extends Button
 
 
-const WHITE_BULLET := "◦"
+## If enabled, the text will be outlined on focus.
+@export var outline_on_focus_enabled := true:
+	set(value):
+		outline_on_focus_enabled = value
+		notify_property_list_changed()
+## The size (in pixels) of the outline.
+@export var outline_size := 2
+
 const BULLET := "•"
 
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	
 	focus_entered.connect(_on_focus_entered)
 	focus_exited.connect(_on_focus_exited)
 
 
+func _validate_property(property: Dictionary) -> void:
+	match property.name:
+		"outline_size":
+			if outline_on_focus_enabled:
+				property.usage = PROPERTY_USAGE_DEFAULT
+			else:
+				property.usage = PROPERTY_USAGE_NO_EDITOR | PROPERTY_USAGE_STORAGE
+
+
 func _set(property: StringName, value: Variant) -> bool:
+	if Engine.is_editor_hint():
+		return false
+	
 	if property == &"text":
 		var button_text := "{symbol} {text}".format({
-			"symbol": WHITE_BULLET,
+			"symbol": BULLET,
 			"text": value,
 		})
 		text = button_text
@@ -23,8 +46,10 @@ func _set(property: StringName, value: Variant) -> bool:
 
 
 func _on_focus_entered() -> void:
-	text = text.replace(WHITE_BULLET, BULLET)
+	if outline_on_focus_enabled:
+		add_theme_constant_override(&"outline_size", outline_size)
 
 
 func _on_focus_exited() -> void:
-	text = text.replace(BULLET, WHITE_BULLET)
+	if outline_on_focus_enabled:
+		remove_theme_constant_override(&"outline_size")
